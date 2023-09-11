d6a566035e70e104c776d256530da7623ea68dda
Muhamad Fiqih Ikhsan <m.fiqihikhsan@gmail.com>
2023-09-12 00:05:17 +0700

    merging done

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run()
-from flask import Flask, render_template, request
-
-app = Flask(__FormOrder__)
-
-@app.route('/order', methods=['GET', 'POST'])
+@app.route('/form', methods=['GET', 'POST'])
 def order():
     if request.method == 'POST':
         # Dapatkan data dari formulir
@@ -26,6 +18,33 @@ def order():
         return f'Terima kasih, {name}! Pesanan Anda untuk {quantity} {product} sudah kami terima.'
     
     return render_template('order_form.html')
+
+
+list_order = [
+    {
+        "nama": "Roy",
+        "email": 'a@a',
+        "produk": "Nasgor",
+        "jumlah": 5
+    },
+    {
+        "nama": "Roy2",
+        "email": 'a@33a',
+        "produk": "N323asgor",
+        "jumlah": 3
+    },
+    {
+        "nama": "Roy3",
+        "email": 'a@add',
+        "produk": "Nasgasdsador",
+        "jumlah": 1
+    },
+]
+
+@app.route('/order')
+def orderPage():
+    return render_template('listOrder.html', list_order=list_order)
+
 
 if __name__ == '__main__':
     app.run()
